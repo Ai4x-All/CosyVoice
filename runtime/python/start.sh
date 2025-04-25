c52b4d048a960d596a99e04b928fe0e4ea2a502f
Jason Wang <27151800+flywhc@users.noreply.github.com>
2025-04-25 12:08:07 +0800

    fix lint

--- runtime/python/start.sh
+++ runtime/python/start.sh
@@ -6,9 +6,9 @@ cd /opt/CosyVoice/CosyVoice
 # 安装依赖
 echo "Installing Python dependencies..."
 pip3 install -r requirements.txt
-cd CosyVoice/runtime/python/grpc && \
-python3 -m grpc_tools.protoc -I. --python_out=. --grpc_python_out=. cosyvoice.proto && \
+cd CosyVoice/runtime/python/grpc
+python3 -m grpc_tools.protoc -I. --python_out=. --grpc_python_out=. cosyvoice.proto
 
 # 启动应用程序
 echo "Starting CosyVoice application..."
-python3 app.py 
+python3 app.py
